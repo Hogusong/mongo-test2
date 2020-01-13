78b3595ef2beabcbb0f95d0ef59b5d5656a14747
“Young <hogusong1@gmail.com>
2020-01-13 16:08:21 -0500

    Fix the middlewares position to be called before or after handling data.

--- app.js
+++ app.js
@@ -10,11 +10,15 @@ if (process.env.NODE_ENV !== 'test') {
   mongoose.connect('mongodb://localhost/muber');
 }
 
+// run middleware befor routes handle
 app.use(bodyParser.json());
-app.use((err, req, res, next) => {
-  res.status(422).send({ error: err.message });
-})
 
 driverRoute(app);
+
+// run middleware after routes handle
+app.use((err, req, res, next) => {
+  console.log(err.message);
+  res.status(422).send({ error: err.message });
+})
 
 module.exports = app;
